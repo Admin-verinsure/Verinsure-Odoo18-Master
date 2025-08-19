0f25816a8f6db2a451a94396369a96e1b59d225d
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2025-08-19 19:49:03 +0530

    addons committed

---
+++ addons/_broken_invoice_format_editor/__manifest__.py
@@ -0,0 +1,8 @@
+{
+            'name': 'Invoice Format Editor',
+                'version': '1.0',
+                    'depends': ['base'],
+                        'data': [],
+                            'installable': True,
+                            }
+
