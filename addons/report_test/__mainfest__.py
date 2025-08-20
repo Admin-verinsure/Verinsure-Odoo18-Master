6fab90bbdecdec67913e74ad82ecead88f044188
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2025-08-20 08:15:17 +0530

    addons committed

---
+++ addons/report_test/__mainfest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "PDF Report Test",
+    'summary': "A simple module to test PDF report rendering.",
+    'version': '1.0',
+    'category': 'Hidden',
+    'depends': ['sale_management'],
+    'data': [
+        'reports/sale_order_report.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
