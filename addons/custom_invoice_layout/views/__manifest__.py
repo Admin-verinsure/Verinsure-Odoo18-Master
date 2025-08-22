07ccb4cf6ab49b3ab613caef89012bd7d34caa8f
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2025-08-22 15:04:22 +0530

    addons committed

---
+++ addons/custom_invoice_layout/views/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Custom Report Layout',
+    'version': '1.0',
+    'category': 'Reporting',
+    'summary': 'Custom external layout inherited from project web.external_layout_standard',
+    'description': """
+This module duplicates Odoo18’s external layout and inherits the project layout 
+(web.external_layout_standard) for applying custom changes without touching original code.
+    """,
+    'author': 'Your Company',
+    'license': 'LGPL-3',
+    'depends': ['web', 'account'],
+    'data': [
+        'views/external_layout_standard.xml',
+        'views/external_layout_inherit.xml',
+        'views/report_invoice_document.xml',
+        'views/custom_invoice_layout.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
