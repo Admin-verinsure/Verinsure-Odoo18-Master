38dab3987f4c2cba434b9e3edaee820c8fe2965d
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2025-08-25 20:37:39 +0530

    Resolved merge conflicts by keeping deletions

--- addons/format_editor/__manifest__.py
+++ addons/format_editor/__manifest__.py
@@ -1,6 +1,6 @@
 
 {
-    'name': 'invoice format editor',
+    'name': 'format editor',
     'version': '18.0.1.0.0',
     'category': 'Accounting',
     'summary': 'Invoice Report, Report Editor, Customise Invoice Report, '
