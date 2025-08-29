c74d744127c452877af32a125f613a8df2988126
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2025-08-29 13:33:54 +0530

    new module

--- addons/signup_club_type/models/res_partner.py
+++ addons/signup_club_type/models/res_partner.py
@@ -1,13 +1,13 @@
 from odoo import models
 
 class ResUsers(models.Model):
-    _inherit = 'res.partner'
+    _inherit = 'res.users'
 
     def _signup_create_user(self, values):
-        """Override signup to also save program_type into partner"""
+        """Override signup to also save club_type into partner"""
         user = super(ResUsers, self)._signup_create_user(values)
-        if values.get('program_type'):
+        if values.get('club_type'):
             user.partner_id.sudo().write({
-                'club_type': values['program_type']
+                'club_type': values['club_type']
             })
         return user
