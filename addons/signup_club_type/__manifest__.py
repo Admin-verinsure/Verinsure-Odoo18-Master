4e528ab06e09c9d82a6b30cb0b484383d74aef62
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2025-09-01 11:38:10 +0530

    new module

--- addons/signup_club_type/__manifest__.py
+++ addons/signup_club_type/__manifest__.py
@@ -1,10 +1,15 @@
 {
     "name": "Signup Program Type (server-rendered)",
     "version": "18.0.1.0",
-    "depends": ["website", "auth_signup"],
-    "data": ["views/signup_form.xml",
-             "views/rotary_id_edit.xml",
-             
-             ],
+    "depends": [
+        "website",
+        "auth_signup",
+        "rotary_project_map",      # 👈 required for rotary_club_id / rotary_membership_id
+        "ldap_reset_password"      # 👈 add this too since you inherit its templates
+    ],
+    "data": [
+        "views/signup_form.xml",
+        "views/rotary_id_edit.xml",
+    ],
     "installable": True,
 }
