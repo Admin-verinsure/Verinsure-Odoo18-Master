89c4a9111a4e25165aaa89b6c6f2fd68c38478ec
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2025-09-10 08:15:40 +0530

    new module

--- addons/custom_contact/__manifest__.py
+++ addons/custom_contact/__manifest__.py
@@ -1,7 +1,14 @@
 {
-    "name": "Contacts – Show/Hide Org vs Membership Tabs",
+    "name": "Contacts – Hide Org/Membership tabs by type",
     "version": "1.0",
-    "depends": ["contacts"],   # plus the module that adds your Rotary/Membership tabs if separate
+    "depends": [
+        "contacts",
+        # also depend on the modules that ADD the tabs so our view loads after them:
+        # e.g. "membership" (or "association") for the Membership tab,
+        # and your custom module that adds "Rotary Org Info"
+        # "membership",
+        # "your_rotary_module",
+    ],
     "data": ["views/res_partner_form.xml"],
     "installable": True,
 }
