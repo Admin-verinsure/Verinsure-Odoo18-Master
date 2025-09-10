f4db3dc65218aad726b217f53d4715b645826276
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2025-09-10 09:25:25 +0530

    new module

--- addons/custom_contact/__manifest__.py
+++ addons/custom_contact/__manifest__.py
@@ -1,7 +1,13 @@
 {
     "name": "Contacts – Toggle Org vs Membership tabs",
     "version": "1.0",
-    "depends": ["contacts", "membership", "rotary_project_map"],
+    "depends": [
+        "contacts",
+        "membership",
+        "rotary_project_map",
+        "membership_rotary_modifications",
+        "signup_club_type",
+    ],
     "data": ["views/res_partner_form.xml"],
     "installable": True,
 }
