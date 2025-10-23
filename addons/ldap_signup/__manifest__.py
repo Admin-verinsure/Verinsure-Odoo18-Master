6d37d7eae7be140822bd782fa52d3be2e29844f4
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2025-10-23 09:10:56 +0530

    new module

--- addons/ldap_signup/__manifest__.py
+++ addons/ldap_signup/__manifest__.py
@@ -1,14 +1,11 @@
 {
-    "name": "LDAP Signup",
-    "version": "18.0.1.0.0",
-    "summary": "Standalone website signup that reuses existing LDAP entries (by email) or creates new ones",
-    "depends": ["website", "auth_signup", "auth_ldap", "ldap_user_utils", "mail"],
-    "data": [
-        "views/signup_templates.xml",   # XML root = <odoo>
-    ],
-    "qweb": [],                         # ensure Odoo doesn't parse it as QWeb
-    "license": "LGPL-3",
-    "installable": True,
-    "application": False,
-    "auto_install": False,
+  "name": "LDAP Signup",
+  "version": "18.0.1.0.0",
+  "summary": "Website signup that reuses existing LDAP entries (by email) or creates new ones",
+  "depends": ["website", "auth_signup", "auth_ldap", "ldap_user_utils", "mail"],
+  "data": [
+    "views/signup_templates.xml"
+  ],
+  "license": "LGPL-3",
+  "installable": True,
 }
