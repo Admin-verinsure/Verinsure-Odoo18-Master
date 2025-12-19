e5da7a0b8a41c5f8c0755c96c20f1bd69c003340
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2025-12-19 06:06:15 +0530

    new module

--- addons/insurance_invoice_rpc/__manifest__.py
+++ addons/insurance_invoice_rpc/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Insurance RPC: Create Insurance + Invoice + Email (Cybro)",
-    "version": "18.0.1.0.7",
+    "version": "18.0.1.0.9",
     "category": "Insurance",
-    "summary": "RPC method to create insurance.details + posted invoice + PDF email; includes invoice form field for insurance link.",
+    "summary": "RPC create insurance.details + posted invoice + PDF email. Matches Cybro Insurance 'Invoices' tab via invoice_origin=INS/xxx.",
     "depends": ["base", "account", "mail", "insurance_management_cybro"],
     "data": [
         "views/account_move_view.xml",
