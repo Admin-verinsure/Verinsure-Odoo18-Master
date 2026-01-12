00a35686f6d3c966aa155220be507245d4669d07
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2026-01-12 09:57:17 +0530

    new module

--- addons/partner_person_name_only/__manifest__.py
+++ addons/partner_person_name_only/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Contacts: Show Only Person Name (Parent Linked)",
-    "version": "18.0.2.0.0",
+    "version": "18.0.2.2.0",
     "category": "Contacts",
     "summary": "For child contacts (persons) linked to a parent company, show only the person's name (no company prefix).",
     "depends": ["contacts"],
