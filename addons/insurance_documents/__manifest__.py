99c45c2ea6f4de2517bace6c7fc9810d322e3a0b
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2026-01-14 06:47:08 +0530

    new module

---
+++ addons/insurance_documents/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Insurance Documents",
+    "version": "18.0.1.0.0",
+    "category": "Insurance",
+    "summary": "Upload and manage documents in Insurance module",
+    "depends": ["base", "mail", "insurance_management_cybro"],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/insurance_documents_views.xml"
+    ],
+    "installable": True,
+    "application": False
+}
