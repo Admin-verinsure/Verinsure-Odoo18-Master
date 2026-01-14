5628f7d1376f7b928bdf0be4f03166bb1d62062f
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2026-01-14 14:23:32 +0530

    new module

--- addons/insurance_documents/models/__init__.py
+++ addons/insurance_documents/models/__init__.py
@@ -1,3 +1,2 @@
 from . import insurance_details
 from . import ir_attachment
-from . import res_users
--- addons/insurance_documents/models/ir_attachment.py
+++ addons/insurance_documents/models/ir_attachment.py
@@ -1,20 +1,7 @@
-
-from odoo import api, fields, models
+from odoo import fields, models
 
 class IrAttachment(models.Model):
     _inherit = "ir.attachment"
 
-    x_insurance_doc = fields.Boolean(default=False)
-    x_club_id = fields.Many2one("res.partner")
-    x_owner_user_id = fields.Many2one("res.users")
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            if vals.get("x_insurance_doc"):
-                vals.setdefault("x_owner_user_id", self.env.user.id)
-            if vals.get("x_insurance_doc") and vals.get("res_model") == "insurance.details" and vals.get("res_id"):
-                policy = self.env["insurance.details"].browse(vals["res_id"])
-                if policy.exists():
-                    vals.setdefault("x_club_id", policy.x_club_id.id or False)
-        return super().create(vals_list)
+    # Marks attachments created/managed via the Insurance Documents module
+    x_insurance_doc = fields.Boolean(string="Insurance Document", index=True, default=False)
