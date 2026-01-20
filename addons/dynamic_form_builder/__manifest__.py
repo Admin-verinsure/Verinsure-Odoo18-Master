e984d2e52c91301e44e62a00d7d4aa0ccdf55e33
Nikhil Rana <nikhil.rana.csit.2021@miet.ac.in>
2026-01-20 22:16:30 +0530

    new module

--- addons/dynamic_form_builder/__manifest__.py
+++ addons/dynamic_form_builder/__manifest__.py
@@ -1,14 +1,14 @@
 {
     "name": "Dynamic Form Builder",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Website",
     "summary": "Form Builder with Dynamic Dropdown Options from Database",
     "depends": ["base", "website"],
     "data": [
         "views/form_views.xml",
         "views/field_views.xml",
-        "views/public_form_template.xml",
+        "views/public_form_template.xml"
     ],
     "installable": True,
-    "application": True,
+    "application": True
 }
