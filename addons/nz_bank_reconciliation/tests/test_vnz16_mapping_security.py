776fd107293c908280d32e5c5554345beb3252a7
Nikhil-Rana-code <rana.nikhil.cse@gmail.com>
2026-08-02 22:09:06 +0530

    secound phase testing resolution

--- addons/nz_bank_reconciliation/tests/test_vnz16_mapping_security.py
+++ addons/nz_bank_reconciliation/tests/test_vnz16_mapping_security.py
@@ -17,6 +17,7 @@ class TestVNZ16MappingSecurity(TransactionCase):
 
         cls.company_a = cls.env.company
         cls.company_b = cls.Company.create({'name': 'VNZ16 Company B'})
+        cls.company_c = cls.Company.create({'name': 'VNZ16 Company C'})
 
         cls.partner_ic = cls.Partner.create({'name': 'IC Partner B'})
         cls.partner_ic_2 = cls.Partner.create({'name': 'IC Partner C'})
@@ -28,6 +29,14 @@ class TestVNZ16MappingSecurity(TransactionCase):
             'email': 'vnz16_non_admin_manager@example.com',
             'company_id': cls.company_a.id,
             'company_ids': [(6, 0, [cls.company_a.id, cls.company_b.id])],
+            'groups_id': [(6, 0, [manager_group.id])],
+        })
+        cls.limited_manager = cls.Users.create({
+            'name': 'VNZ16 Limited Manager',
+            'login': 'vnz16_limited_manager',
+            'email': 'vnz16_limited_manager@example.com',
+            'company_id': cls.company_a.id,
+            'company_ids': [(6, 0, [cls.company_a.id])],
             'groups_id': [(6, 0, [manager_group.id])],
         })
 
@@ -44,6 +53,47 @@ class TestVNZ16MappingSecurity(TransactionCase):
         self.assertEqual(mapping.company_id.id, self.company_a.id)
         self.assertEqual(mapping.partner_id.id, self.partner_ic.id)
         self.assertEqual(mapping.counterpart_company_id.id, self.company_b.id)
+
+    def test_create_succeeds_with_authorized_companies(self):
+        mapping = self.Mapping.with_user(self.non_admin_manager).create({
+            'company_id': self.company_a.id,
+            'partner_id': self.partner_ic.id,
+            'counterpart_company_id': self.company_b.id,
+        })
+        self.assertTrue(mapping.exists())
+
+    def test_create_rejects_unauthorized_company_id(self):
+        with self.assertRaisesRegex(
+            ValidationError,
+            'You are not allowed to create mappings for company id',
+        ):
+            self.Mapping.with_user(self.limited_manager).create({
+                'company_id': self.company_b.id,
+                'partner_id': self.partner_ic.id,
+                'counterpart_company_id': self.company_a.id,
+            })
+
+    def test_create_rejects_unauthorized_counterpart_company_id(self):
+        with self.assertRaisesRegex(
+            ValidationError,
+            'You are not allowed to use counterpart company id',
+        ):
+            self.Mapping.with_user(self.limited_manager).create({
+                'company_id': self.company_a.id,
+                'partner_id': self.partner_ic.id,
+                'counterpart_company_id': self.company_b.id,
+            })
+
+    def test_create_rejects_same_company_and_counterpart(self):
+        with self.assertRaisesRegex(
+            ValidationError,
+            'This Company and Counterpart Company must be different.',
+        ):
+            self.Mapping.with_user(self.non_admin_manager).create({
+                'company_id': self.company_a.id,
+                'partner_id': self.partner_ic.id,
+                'counterpart_company_id': self.company_a.id,
+            })
 
     def test_notes_can_be_updated(self):
         mapping = self._create_mapping()
