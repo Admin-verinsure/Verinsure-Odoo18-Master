6e6d6094f4c5826bd23d08b3d36bf9a160f0bd5f
Nikhil-Rana-code <rana.nikhil.cse@gmail.com>
2026-08-03 14:06:27 +0530

    secound phase testing resolution

---
+++ addons/session_cookie_hardening/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from .hooks import post_load_hook
---
+++ addons/session_cookie_hardening/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Session Cookie Hardening",
+    "summary": "VNZ-17 session_id cookie hardening",
+    "version": "18.0.1.0.0",
+    "category": "Technical",
+    "license": "LGPL-3",
+    "depends": ["web"],
+    "post_load": "post_load_hook",
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
